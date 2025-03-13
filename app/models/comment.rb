23283d129bf9bd21c3d34b59cd0e869927b534ab
Jelani Woods <jelani@firstdraft.com>
2025-03-13 11:50:48 -0500

    TMP - render doesn't like these callbacks for some reason

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ApplicationRecord
 
   scope :default_order, -> { order(created_at: :asc) }
 
-  after_create_commit -> { broadcast_before_to "comments_photo_#{photo_id}", target: "comments_form_photo_#{photo_id}", partial: "comments/placeholder", locals: { comment: self } }
-  after_update_commit -> { broadcast_replace_to "comment_#{id}", partial: "comments/placeholder", locals: { comment: self } }
-  after_destroy_commit -> { broadcast_remove_to "comment_#{id}" }
+  # after_create_commit -> { broadcast_before_to "comments_photo_#{photo_id}", target: "comments_form_photo_#{photo_id}", partial: "comments/placeholder", locals: { comment: self } }
+  # after_update_commit -> { broadcast_replace_to "comment_#{id}", partial: "comments/placeholder", locals: { comment: self } }
+  # after_destroy_commit -> { broadcast_remove_to "comment_#{id}" }
 end
