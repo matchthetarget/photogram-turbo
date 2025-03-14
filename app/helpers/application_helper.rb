fd54191c535e11e12f5f0b0b09550ff4971afcad
Jelani Woods <jelani@firstdraft.com>
2025-03-14 12:30:04 -0500

    Show underline on hover for links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if input_url.present?
       url = URI(input_url)
 
-      link_to url.host + url.path, input_url
+      link_to url.host + url.path, input_url, class: "link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
     else
       nil
     end
